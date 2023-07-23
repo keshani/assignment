1293e999cddac080d4691c378cc17ab5edb04689
keshani <keshani.rathnayaka@gmail.com>
2023-07-23 08:54:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# assignment
+ boku assignment
---
+++ src/test/java/com/boku/assignment/message/MessageProducerImplTest.java
@@ -0,0 +1,32 @@
+package com.boku.assignment.message;
+
+import com.boku.assignment.model.ClientNumberRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MessageProducerImplTest {
+
+    @Autowired
+    private MessageQueueImpl messageQueue;
+
+    @Autowired
+    private MessageProducerImpl messageProducer;
+
+    @Test
+    void test_msg_send() {
+        ClientNumberRequest request = new ClientNumberRequest("5");
+        try {
+            messageProducer.send(request);
+            ClientNumberRequest result =  messageQueue.readMessageNonBlocking();
+            assertEquals(request.getId(), result.getId());
+            assertEquals("5", result.getSendValue());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
